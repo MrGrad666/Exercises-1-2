8178e7b519427c1fa15033755b325252938aa2fe
Andrew <125542028+MrGrad666@users.noreply.github.com>
2023-02-16 22:44:01 +0300

    Add files via upload

---
+++ Program Exercise1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PI number - 3,141592653
+
+            //Eiler number - 2,7182818284590452
+
+            decimal p = +3.141592653m;
+            decimal e = +2.7182818284590452m;
+            Console.WriteLine(p);
+            Console.WriteLine(e);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Program Exercise2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_Exercise_2_
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            string a = "n моя строка 1";
+            string b = "t моя строка 2";
+            string c = "a моя строка 3";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
